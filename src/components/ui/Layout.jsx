85bb0c0a9ab227be4f1936092eec4bf394842fd8
sevpawl <savannahpawl@icloud.com>
2025-03-26 19:27:53 +0000

    feat(UI): make basic Layout component

---
+++ src/components/ui/Layout.jsx
@@ -0,0 +1,13 @@
+import Header from './Header';
+import Card from './Card';
+
+const Layout = () => {
+  return (
+    <div className='mx-12'>
+      <Header />
+      <Card />
+    </div>
+  );
+};
+
+export default Layout;
