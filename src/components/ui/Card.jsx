602a737b1b0f0f4c8c64a91ff8c8a92d92e428b3
sevpawl <savannahpawl@icloud.com>
2025-03-27 20:32:40 +0000

    style(Card): update colors

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -2,8 +2,8 @@ import '../../global.css';
 
 const Card = () => {
   return (
-    <div className="w-72 h-72 bg-gray-200 flex items-center justify-center rounded-3xl py-3 px-3 shadow-[8px_8px_12px_#b8b8b8,-8px_-8px_16px_#ffff]">
-      <div className=" w-64 h-64 py-3 px-3 bg-gray-200 rounded-2xl shadow-[inset_4px_4px_8px_#00000033,_inset_-4px_-4px_8px_#ffffff80]" />
+    <div className="w-72 h-72 bg-neutral-200 flex items-center justify-center rounded-3xl py-3 px-3 shadow-[8px_8px_12px_#b8b8b8,-8px_-8px_16px_#ffff]">
+      <div className=" w-64 h-64 py-3 px-3 bg-neutral-200 rounded-2xl shadow-[inset_4px_4px_8px_#00000033,_inset_-4px_-4px_8px_#ffffff80]" />
     </div>
   );
 };
