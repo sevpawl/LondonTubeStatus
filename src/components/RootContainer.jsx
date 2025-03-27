2d271451deda5bc5eb5be83066b1d114ffdc714e
sevpawl <savannahpawl@icloud.com>
2025-03-27 20:39:59 +0000

    feat(UI): integrate LineStatusTile into Layout component, adjust alignment

--- src/components/RootContainer.jsx
+++ src/components/RootContainer.jsx
@@ -1,5 +1,6 @@
 import Header from './ui/Header';
 import Card from './ui/Card';
+import LineStatusTile from './lines/LineStatusTile';
 import Layout from './ui/Layout';
 
-export { Header, Card, Layout };
+export { Header, Card, LineStatusTile, Layout };
--- src/components/ui/Layout.jsx
+++ src/components/ui/Layout.jsx
@@ -1,10 +1,15 @@
-import { Header, Card } from '../RootContainer';
+import { Header, Card, LineStatusTile } from '../RootContainer';
 
 const Layout = () => {
   return (
-    <div className="mx-12">
+    <div className="mx-12 flex flex-col items-center">
       <Header />
       <Card />
+      <div className="grid grid-rows-4 gap-6 max-w-[900px] w-full">
+        <LineStatusTile></LineStatusTile>
+        <LineStatusTile></LineStatusTile>
+        <LineStatusTile></LineStatusTile>
+      </div>
     </div>
   );
 };
