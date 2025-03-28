ee9b1335946413b7106593734c666ec04c47ca67
sevpawl <savannahpawl@icloud.com>
2025-03-28 13:55:24 +0000

    feat(LineStatusTile): add title, color, status props

--- src/components/lines/LineStatusTile.jsx
+++ src/components/lines/LineStatusTile.jsx
@@ -1,11 +1,12 @@
 import '../../global.css';
 
-const LineStatusTile = () => {
+const LineStatusTile = ({ title, color, status }) => {
   return (
-    <div className=" aboslute w-200px bg-neutral-200 rounded-full mt-10 py-3 px-3 shadow-[8px_8px_16px_#b8b8b8,-8px_-8px_16px_#ffff]">
-      <div className="relative bg-neutral-200 rounded-full py-4 px-6 shadow-[inset_4px_4px_8px_#00000033,_inset_-4px_-4px_8px_#ffffff80] flex items-center w-full justify-start">
-        <span className="text-gray-500 font-extrabold text-medium font-stretch-105% ml-4">
-          line status tile
+    <div className=" aboslute w-200px bg-neutral-200 rounded-full py-3 px-3 shadow-[8px_8px_16px_#b8b8b8,-8px_-8px_16px_#ffff]">
+      <div className="relative bg-neutral-200 rounded-full overflow-hidden shadow-[inset_4px_4px_8px_#00000033,_inset_-4px_-4px_8px_#ffffff80] flex items-center w-full justify-start">
+        <div className="h-8 w-8 ml-4 rounded-full " style={{ backgroundColor: color }} />
+        <span className="text-gray-500 py-4 pl-6 font-extrabold text-medium font-stretch-105% ml-4">
+          {title}
         </span>
       </div>
     </div>
