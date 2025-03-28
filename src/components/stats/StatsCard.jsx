d94a0c2da15d985f0c03bfe2d7f019c5846fd3c8
sevpawl <savannahpawl@icloud.com>
2025-03-28 15:13:18 +0000

    feat(UI): add children prop to global Card component, make baisc AlertsCard, ChartCard, and StatsCard components

---
+++ src/components/stats/StatsCard.jsx
@@ -0,0 +1,11 @@
+import '../../global.css';
+
+const StatsCard = ({ title }) => {
+  return (
+        <div className="text-4xl not-first:text-neutral-100/80 font-extrabold drop-shadow-[_-3px_-3px_6px_#ffffff80,_4px_4px_2px_#00000033] mt-2 ">
+          alerts
+        </div>
+  );
+};
+
+export default StatsCard;
