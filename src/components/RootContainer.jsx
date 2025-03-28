b84605da37fd3606e15988f13b8e04d825b09b3b
sevpawl <savannahpawl@icloud.com>
2025-03-28 13:26:58 +0000

    refactor(RootContainer): remove Layout export from RootContainer component to fix circular dependency error

--- src/components/RootContainer.jsx
+++ src/components/RootContainer.jsx
@@ -1,6 +1,5 @@
 import Header from './ui/Header';
 import Card from './ui/Card';
 import LineStatusTile from './lines/LineStatusTile';
-import Layout from './ui/Layout';
 
-export { Header, Card, LineStatusTile, Layout };
+export { Header, Card, LineStatusTile };
