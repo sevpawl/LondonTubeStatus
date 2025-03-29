3c1a93332cb73625b49d051a2d5a9ce0383c4d03
sevpawl <savannahpawl@icloud.com>
2025-03-29 16:04:48 +0000

    feat(LineStatusTile): add LineStatusBadge to root container and integrate into LineStatusTile component

--- src/components/RootContainer.jsx
+++ src/components/RootContainer.jsx
@@ -1,8 +1,10 @@
 import Header from './ui/Header';
 import Card from './ui/Card';
 import LineStatusTile from './lines/LineStatusTile';
+import LineStatusBadge from './lines/LineStatusBadge';
 import StatsCard from './stats/StatsCard';
 import AlertsCard from './stats/AlertsCard';
 import ChartCard from './stats/ChartCard';
 
-export { Header, Card, LineStatusTile, StatsCard, AlertsCard, ChartCard};
+
+export { Header, Card, LineStatusTile, LineStatusBadge, StatsCard, AlertsCard, ChartCard};
--- src/components/lines/LineStatusTile.jsx
+++ src/components/lines/LineStatusTile.jsx
@@ -1,13 +1,22 @@
 import '../../global.css';
+import LineStatusBadge from './LineStatusBadge';
 
-const LineStatusTile = ({ title, color, status }) => {
+const LineStatusTile = ({ title, color }) => {
   return (
-    <div className=" aboslute w-200px bg-neutral-200 rounded-full py-3 px-3 shadow-[8px_8px_16px_#b8b8b8,-8px_-8px_16px_#ffff]">
-      <div className="relative bg-neutral-200 rounded-full overflow-hidden shadow-[inset_4px_4px_8px_#00000033,_inset_-4px_-4px_8px_#ffffff80] flex items-center w-full justify-start">
-        <div className="h-8 w-8 ml-4 rounded-full " style={{ backgroundColor: color }} />
-        <span className="text-gray-500 py-4 pl-6 font-extrabold text-medium font-stretch-105% ml-4">
-          {title}
-        </span>
+    <div className="w-full bg-neutral-200 rounded-full py-3 px-3 shadow-[8px_8px_16px_#b8b8b8,-8px_-8px_16px_#ffffff] cursor-pointer transition-all duration-300">
+      <div className="bg-neutral-200 rounded-full overflow-hidden shadow-[inset_4px_4px_8px_#00000033,_inset_-4px_-4px_8px_#ffffff80] flex items-center justify-between w-full">
+        <div className="flex items-center">
+          <div
+            className="h-8 w-8 ml-4 rounded-full border-4 border-white flex-shrink-0"
+            style={{ backgroundColor: color }}
+          />
+          <span className="text-gray-600 py-4 pl-6 font-extrabold text-medium font-stretch-105% ml-2">
+            {title}
+          </span>
+        </div>
+        <div className="flex items-center">
+          <LineStatusBadge></LineStatusBadge>
+        </div>
       </div>
     </div>
   );
