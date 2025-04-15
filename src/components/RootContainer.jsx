648aabd968de3b3b98a43a5f1384714d0b9c5793
sevpawl <savannahpawl@icloud.com>
2025-04-15 12:47:26 +0100

    feat(RootContainer): import Modal component and update exports

--- src/components/RootContainer.jsx
+++ src/components/RootContainer.jsx
@@ -5,6 +5,7 @@ import LineStatusBadge from './lines/LineStatusBadge';
 import StatsCard from './stats/StatsCard';
 import AlertsCard from './stats/AlertsCard';
 import ChartCard from './stats/ChartCard';
+import Modal from './ui/Modal';
 
 
-export { Header, Card, LineStatusTile, LineStatusBadge, StatsCard, AlertsCard, ChartCard};
+export { Header, Card, LineStatusTile, LineStatusBadge, StatsCard, AlertsCard, ChartCard, Modal };
