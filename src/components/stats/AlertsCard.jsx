bf1db2d50eaaa2e6c02f2b6c97a8d7b61278e4c6
sevpawl <savannahpawl@icloud.com>
2025-04-24 18:06:58 +0100

    style(AlertsCard): update layout and content for alerts display

--- src/components/stats/AlertsCard.jsx
+++ src/components/stats/AlertsCard.jsx
@@ -2,19 +2,14 @@ import '../../global.css';
 
 const AlertsCard = ( lineColor ) => {
   return (
-    <div className="cursor-default">
-      <span className="text-3xl ml-12 not-first:text-neutral-100/80 font-extrabold drop-shadow-[_-3px_-3px_6px_#ffffff80,_4px_4px_2px_#00000033] mt-2">
-        alert
+    <div className="cursor-default overflow-scroll mx-6 my-2">
+      <span className="text-3xl ml-20 not-first:text-neutral-100/80 font-extrabold drop-shadow-[_-3px_-3px_6px_#ffffff80,_4px_4px_2px_#00000033]">
+        alerts
       </span>
       <div className="grid">
-        <div className="w-50 h-8 mt-5 pl-6 pt-0.5 rounded-2xl overflow-hidden bg-red-500/80 text-white font-extrabold text-medium font-stretch-105%">
-          alert
-        </div>
-        <div className="w-50 h-8 mt-5 pl-6 pt-0.5 rounded-2xl overflow-hidden bg-red-500/80 text-white font-extrabold text-medium font-stretch-105%">
-          alert
-        </div>
-        <div className="w-50 h-8 mt-5 pl-6 pt-0.5 rounded-2xl overflow-hidden bg-red-500 text-white font-extrabold text-medium font-stretch-105%">
-          alert
+        <div className="mt-5 px-3 py-2 rounded-2xl overflow-hidden bg-red-500/80 text-white font-extrabold text-medium font-stretch-105%">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua.
         </div>
       </div>
     </div>
