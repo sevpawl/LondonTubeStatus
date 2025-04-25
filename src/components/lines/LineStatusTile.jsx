77dab10ef7ed6e18e97b0e8995c15e7265a9f3fd
sevpawl <savannahpawl@icloud.com>
2025-04-25 12:51:45 +0100

    style(LineStatusTile): refine layout and spacing for improved aesthetics

--- src/components/lines/LineStatusTile.jsx
+++ src/components/lines/LineStatusTile.jsx
@@ -3,14 +3,14 @@ import LineStatusBadge from './LineStatusBadge';
 
 const LineStatusTile = ({ title, lineColor }) => {
   return (
-    <div className="w-full bg-neutral-200 rounded-full py-3 px-3 shadow-[8px_8px_16px_#b8b8b8,-8px_-8px_16px_#ffffff] hover:shadow-[2px_2px_4px_#b8b8b8,-2px_-2px_4px_#ffffff] transition-all duration-300">
-      <div className="bg-neutral-200 rounded-full overflow-hidden shadow-[inset_4px_4px_8px_#00000033,_inset_-4px_-4px_8px_#ffffff80] flex items-center justify-between w-full">
+    <div className="w-full bg-neutral-200 rounded-full p-2 shadow-[8px_8px_16px_#b8b8b8,-8px_-8px_16px_#ffffff] hover:shadow-[2px_2px_4px_#b8b8b8,-2px_-2px_4px_#ffffff] transition-all duration-300">
+      <div className="bg-neutral-200 rounded-full overflow-hidden shadow-[inset_4px_4px_8px_#00000033,_inset_-4px_-4px_8px_#ffffff80] flex items-center justify-between w-full px-4 py-2">
         <div className="flex items-center">
           <div
-            className="h-8 w-8 ml-4 rounded-full border-4 border-white flex-shrink-0"
+            className="h-8 w-8 rounded-full border-4 border-white flex-shrink-0"
             style={{ backgroundColor: lineColor }}
           />
-          <span className="text-gray-600 py-4 pl-6 font-extrabold text-medium font-stretch-105% ml-2 cursor-default">
+          <span className="text-gray-600 pl-4 font-extrabold text-medium cursor-default">
             {title}
           </span>
         </div>
