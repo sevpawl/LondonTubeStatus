18cbf1480d3fde881578981ba341d9ecd173b1ee
sevpawl <savannahpawl@icloud.com>
2025-05-01 13:06:51 +0100

    feat(api): add base fot countStatusDisruptions function to get status severity, descriptions, and tally each

--- src/services/api.js
+++ src/services/api.js
@@ -15,4 +15,46 @@ function fetchTubeStatus(mode = 'tube') {
       console.log('error: ', error);
     });
 }
-export default fetchTubeStatus;
+
+// process data
+
+// get status severity and description
+
+// count disruptions
+
+function countStatusDisruptions(data) {
+  const statusCounts = {
+    goodService: 0,
+    minorDelays: 0,
+    severeDelays: 0,
+    partClosure: 0,
+    planned: 0,
+    suspended: 0,
+  };
+
+  const statusSeverity = lineStatuses[0].statusSeverity;
+  const statusDescription = lineStatuses[0].statusSeverityDescription;
+
+  // make status descriptions lowercase
+  const statusDescriptionLower = statusDescription.lower();
+
+  if (statusDescriptionLower.includes('good service')) {
+    goodService += 1;
+  } else if (statusDescriptionLower.includes('minor delays')) {
+    minorDelays += 1;
+  } else if (statusDescriptionLower.includes('severe delays')) {
+    severeDelays += 1;
+  } else if (statusDescriptionLower.includes('part closure')) {
+    partClosure += 1;
+  } else if (statusDescriptionLower.includes('planned')) {
+    planned += 1;
+  } else if (statusDescriptionLower.includes('suspended')) {
+    suspended += 1;
+  }
+
+  console.log('good service: ', goodService);
+  console.log('minor delays: ', minorDelays);
+  return statusCounts;
+}
+
+export default { fetchTubeStatus, countStatusDisruptions };
