634f4db9d1d3a6a0ee58a9b3b4e791387c7f89db
sevpawl <savannahpawl@icloud.com>
2025-05-02 16:36:57 +0100

    feat(Colors): add util file to centralise colors, add status indicator colors for service status types

---
+++ src/utils/Colors.jsx
@@ -0,0 +1,12 @@
+// status indicator colors
+
+const statusColors = {
+  goodService: 'bg-green-500',
+  minorDelays: 'bg-yellow-300',
+  severeDelays: 'bg-red-500',
+  partClosure: 'bg-red-600',
+  planned: 'bg-purple-500',
+  suspended: 'bg-black',
+};
+
+export default statusColors;
