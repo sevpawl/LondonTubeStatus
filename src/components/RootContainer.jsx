c59d4e7ea3e1459df9fccf3e0796aef3e83d54e9
sevpawl <savannahpawl@icloud.com>
2025-05-02 16:28:44 +0100

    refactor(StatsCard): rename for clarity

--- src/components/RootContainer.jsx
+++ src/components/RootContainer.jsx
@@ -2,7 +2,7 @@ import Header from './ui/Header';
 import Card from './ui/Card';
 import LineStatusTile from './lines/LineStatusTile';
 import LineStatusBadge from './lines/LineStatusBadge';
-import StatsCard from './stats/StatsCard';
+import StatsCard from './stats/StatusCountCard';
 import AlertsCard from './stats/AlertsCard';
 import ChartCard from './stats/ChartCard';
 import Modal from './ui/Modal';
