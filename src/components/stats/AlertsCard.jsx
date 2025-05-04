aff42e8a609f4b5fa2080e22628f923ba121dc74
sevpawl <savannahpawl@icloud.com>
2025-05-04 06:56:08 +0100

    refactor(AlertsCard): display alerts in alerts card

--- src/components/stats/AlertsCard.jsx
+++ src/components/stats/AlertsCard.jsx
@@ -21,16 +21,15 @@ const AlertsCard = (lineColor) => {
   }, []);
 
   return (
-    <div className="cursor-default overflow-auto w-full h-full px-4 py-2">
+    <div className="cursor-default w-full h-full px-4 py-2">
       <div className="flex items-center justify-center mb-4 md:mb-6">
         <span className="text-2xl md:text-3xl not-first:text-neutral-100/80 font-extrabold drop-shadow-[_-3px_-3px_6px_#ffffff80,_4px_4px_2px_#00000033]">
           alerts
         </span>
       </div>
-      <div className="grid gap-3">
-        <div className="px-3 py-2 rounded-2xl overflow-hidden bg-red-500/80 text-white font-extrabold text-sm md:text-base">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua.
+      <div className="grid gap-3 overflow-y-auto max-h-[200px]">
+        <div className="px-3 py-2 rounded-2xl bg-red-500/80 text-white font-extrabold text-sm text-md">
+          {descriptions}
         </div>
       </div>
     </div>
