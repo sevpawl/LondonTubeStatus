4850e8128c511074662e3333542eb0d032eedf04
sevpawl <savannahpawl@icloud.com>
2025-05-04 07:05:39 +0100

    feat(Colors): add line colors for tube lines to Colors utility

--- src/utils/Colors.jsx
+++ src/utils/Colors.jsx
@@ -9,4 +9,22 @@ const statusColors = {
   suspended: 'bg-black',
 };
 
-export default statusColors;
+// line colors
+
+const lineColors = {
+  bakerloo: 'bg-[#b44a0b]',
+  central: 'bg-[#e02b24]',
+  circle: 'bg-[#ffcc00]',
+  district: 'bg-[#008039]',
+  elizabeth: 'bg-[#7055a6]',
+  hammersmithCity: 'bg-[#d79caf]',
+  jubilee: 'bg-[#9ea7aa]',
+  metropolitan: 'bg-[#a10060]',
+  northern: 'bg-[#1a1a1a]',
+  overground: 'bg-[#f17a10]',
+  piccadilly: 'bg-[#003b8f]',
+  victoria: 'bg-[#00a3e0]',
+  waterlooCity: 'bg-[#99d1bf]',
+};
+
+export { statusColors, lineColors };
