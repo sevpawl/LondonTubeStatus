20fc70498852687190851fcabcd4409a6eaae7b7
sevpawl <savannahpawl@icloud.com>
2025-05-22 11:59:21 +0100

    refactor(LineStatusBadge): also change cursor to mach function

--- src/components/lines/LineStatusBadge.jsx
+++ src/components/lines/LineStatusBadge.jsx
@@ -5,7 +5,7 @@ const LineStatusBadge = ({ status, statusColor, onClick }) => {
   return (
     <div className="flex items-center">
       <div
-        className="px-2 md:px-3 py-0.5 ml-2 mr-2 md:mr-5 bg-red-500 rounded-full text-xs md:text-sm font-medium cursor-default"
+        className={`px-2 md:px-3 py-0.5 ml-2 mr-2 md:mr-5 bg-black/10 rounded-full text-xs md:text-sm font-medium cursor-pointer`}
         style={{
           boxShadow: 'inset 2px 2px 4px #f8f8f, inset -2px -2px 4px #ffffff',
         }}
