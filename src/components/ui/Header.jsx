ae53068c7e52503a823143515a9ba164a03bb006
sevpawl <savannahpawl@icloud.com>
2025-05-22 17:07:19 +0100

    feat(Header): implement current time display and integrate with page refresh functionality

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,11 +1,23 @@
 import '../../global.css';
+import { useState, useEffect } from 'react';
+import { getCurrentTime } from '../../services/lastUpdated';
 import Clock from './Clock';
+import { Train } from 'lucide-react';
 
 const Header = () => {
+  const [currentTime, setCurrentTime] = useState('');
+
+  useEffect(() => {
+    setCurrentTime(getCurrentTime());
+    console.log('current time: ', currentTime);
+  }, []);
+
   const HandleRefresh = () => {
     console.log('about to reload page');
     window.location.reload();
     console.log('page reloaded');
+    setCurrentTime(getCurrentTime());
+    console.log('current time: ', currentTime);
   };
 
   return (
@@ -15,15 +27,18 @@ const Header = () => {
         <h1 className="text-3xl md:text-4xl lg:text-6xl not-first:text-neutral-100/80 font-extrabold drop-shadow-[_-3px_-3px_6px_#ffffff80,_4px_4px_2px_#00000033] cursor-default text-center md:text-left">
           underground status
         </h1>
+
         <div className="flex items-center">
           <div className="text-center md:text-left">
-            <div className="font-small font-bold white">last updated: </div>
             <button
               onClick={HandleRefresh}
               className="mt-2 md:mt-4 px-4 py-1.5 rounded-lg bg-neutral-200 text-gray-600 font-extrabold text-small cursor-pointer shadow-[4px_4px_8px_#b8b8b8,-4px_-4px_8px_#ffffff] hover:shadow-[2px_2px_4px_#b8b8b8,-2px_-2px_4px_#ffffff] active:shadow-[inset_2px_2px_4px_#b8b8b8,inset_-2px_-2px_4px_#ffffff] transition-all duration-500"
             >
               refresh
             </button>
+            <div className="font-small font-bold white">
+              last updated: {currentTime}
+            </div>
           </div>
         </div>
       </header>
