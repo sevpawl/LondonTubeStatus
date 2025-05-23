2ec381267dd0685256f576fbe45b3613e7dea30d
sevpawl <savannahpawl@icloud.com>
2025-05-23 18:15:07 +0100

    refactor(StatusCountCardItems): formatting improvement

--- src/components/stats/StatusCountCardItems.jsx
+++ src/components/stats/StatusCountCardItems.jsx
@@ -1,8 +1,10 @@
 const StatusCountCardItems = ({ icon, statusType, statusCount, bgColor }) => {
-    return (
-      <div className="w-full mb-2.5 p-2 rounded-lg flex items-center justify-between bg-neutral-100/80 transition-transform hover:-translate-y-0.5 hover:shadow-lg group cursor-pointer">
-        <div className="flex items-center space-x-3">
-        <div className={`flex items-center justify-center ${bgColor} rounded-full w-7 h-7 mr-1 group-hover:scale-110 transition-transform ml-2`}>
+  return (
+    <div className="w-full mb-2.5 p-2 rounded-lg flex items-center justify-between bg-neutral-100/80 transition-transform hover:-translate-y-0.5 hover:shadow-lg group cursor-pointer">
+      <div className="flex items-center space-x-3">
+        <div
+          className={`flex items-center justify-center ${bgColor} rounded-full w-7 h-7 mr-1 group-hover:scale-110 transition-transform ml-2`}
+        >
           {icon}
         </div>
         <span className="text-gray-600 font-semibold text-sm lowercase px-6">
