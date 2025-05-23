761e180f5c764fb61162628f2873c05612eb4e4d
sevpawl <savannahpawl@icloud.com>
2025-05-23 17:55:47 +0100

    feat(StatusCountCardItems): add new component for displaying status counts with icons and styling

---
+++ src/components/stats/StatusCountCardItems.jsx
@@ -0,0 +1,19 @@
+const StatusCountCardItems = ({ icon, statusType, statusCount }) => {
+  return (
+    <div className="w-full mb-2.5 p-2 rounded-lg flex items-center justify-between bg-neutral-100/80 transition-transform hover:-translate-y-0.5 hover:shadow-lg group cursor-pointer">
+      <div className="flex items-center space-x-3">
+        <div className="flex items-center justify-center bg-green-200 rounded-full w-7 h-7 mr-1 group-hover:scale-110 transition-transform ml-2">
+          {icon}
+        </div>
+        <span className="text-gray-600 font-semibold text-sm lowercase px-6">
+          {statusType}
+        </span>
+      </div>
+      <span className="text-gray-600 font-bold text-sm lowercase mr-2">
+        {statusCount}
+      </span>
+    </div>
+  );
+};
+
+export default StatusCountCardItems;
