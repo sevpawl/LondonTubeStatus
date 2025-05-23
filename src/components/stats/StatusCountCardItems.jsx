b85bf72d64c0bcb0e07beb8e1b4c0f39c1fac278
sevpawl <savannahpawl@icloud.com>
2025-05-23 18:12:54 +0100

    feat(StatusCountCardItems): add bgColor prop for icon bg color

--- src/components/stats/StatusCountCardItems.jsx
+++ src/components/stats/StatusCountCardItems.jsx
@@ -1,8 +1,8 @@
-const StatusCountCardItems = ({ icon, statusType, statusCount }) => {
-  return (
-    <div className="w-full mb-2.5 p-2 rounded-lg flex items-center justify-between bg-neutral-100/80 transition-transform hover:-translate-y-0.5 hover:shadow-lg group cursor-pointer">
-      <div className="flex items-center space-x-3">
-        <div className="flex items-center justify-center bg-green-200 rounded-full w-7 h-7 mr-1 group-hover:scale-110 transition-transform ml-2">
+const StatusCountCardItems = ({ icon, statusType, statusCount, bgColor }) => {
+    return (
+      <div className="w-full mb-2.5 p-2 rounded-lg flex items-center justify-between bg-neutral-100/80 transition-transform hover:-translate-y-0.5 hover:shadow-lg group cursor-pointer">
+        <div className="flex items-center space-x-3">
+        <div className={`flex items-center justify-center ${bgColor} rounded-full w-7 h-7 mr-1 group-hover:scale-110 transition-transform ml-2`}>
           {icon}
         </div>
         <span className="text-gray-600 font-semibold text-sm lowercase px-6">
