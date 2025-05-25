3502ea884ea04527a6fa95109484fd7f93aba850
sevpawl <savannahpawl@icloud.com>
2025-05-25 14:52:44 +0100

    refactor(Modal): fix structure and set up state management

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -1,17 +1,22 @@
-import { useState, useEffect } from 'react';
 import '../../global.css';
 
-function Modal({ title, showModal, hideModal, handleOpenModal, handleCloseModal }) {
+function Modal({ title, children,  handleCloseModal }) {
   return (
-    <div className="fixed inset-0 flex items-center justify-center p-4">
-      <div className="relative z-50 w-full max-w-md">
-        <div className="bg-red-500/70 backdrop-blur-sm overflow-hidden border-8 border-white-500 rounded-xl p-4 md:p-6 cursor-default">
-          <div className="flex flex-col items-center justify-center">
-            <div className="text-xl md:text-2xl font-extrabold text-white mb-4">{title}</div>
-            <div className="text-base md:text-lg text-white font-bold text-center">
-              status description goes here status description goes here status description goes here
-            </div>
+    <div>
+      <div className="bg-red-500/70 backdrop-blur-sm overflow-hidden border-8 border-white-500 rounded-xl p-4 md:p-6 cursor-default">
+        <div className="flex flex-col items-center justify-center">
+          <div className="text-xl md:text-2xl font-extrabold text-white mb-4">
+            {title}
           </div>
+          <div className="text-base md:text-lg text-white font-bold text-center">
+            {children}
+          </div>
+          <button
+            className="bg-white py-[0.5px] px-2 mt-4 mb-[-8px] rounded-full text-black cursor-pointer text-xs font-bold"
+            onClick={handleCloseModal}
+          >
+            button
+          </button>
         </div>
       </div>
     </div>
@@ -20,19 +25,6 @@ function Modal({ title, showModal, hideModal, handleOpenModal, handleCloseModal 
 
 export default Modal;
 
-// useState/Effect are for handling open / close later ↓
-
 // handleOutsideClick
-
-// modal control:
-
-// const [showModal] = useState(false);
-// const [handleOpenModal] () => {
-//     console.log('opening modal');
-//      setShowModal(true);
-// };
-// const handleCloseModal = () => {
-//   setShowModal(false);
-// };
 
 // make bg reflect color of status type
