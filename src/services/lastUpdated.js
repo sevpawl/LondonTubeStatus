ec800aa849665be7d9f499d0ddbc32469c024fb7
sevpawl <savannahpawl@icloud.com>
2025-05-28 12:08:31 +0100

    refactor(lastUpdated): change format of timestamp - (hours:minutes:seconds)

--- src/services/lastUpdated.js
+++ src/services/lastUpdated.js
@@ -1,7 +1,11 @@
 function getCurrentTime() {
-  const now = new Date().toISOString();
-  console.log('current time: ', now);
-  return now;
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+  const formattedTime = `${hours}:${minutes}:${seconds}`;
+  console.log('current time: ', formattedTime);
+  return formattedTime;
 }
 
 export { getCurrentTime };
