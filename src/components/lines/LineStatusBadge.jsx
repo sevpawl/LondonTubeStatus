630fd6730d95f5fb97d321271103819f96d3118d
sevpawl <savannahpawl@icloud.com>
2025-05-29 12:32:53 -0400

    style(LineStatusBadge): update buttons

--- src/components/lines/LineStatusBadge.jsx
+++ src/components/lines/LineStatusBadge.jsx
@@ -43,7 +43,7 @@ const LineStatusBadge = ({ statusColor, children }) => {
   return (
     <div className="flex items-center">
       <button
-        className={`px-2 md:px-3 py-0.5 ml-2 mr-2 md:mr-5 bg-${statusColor} rounded-full text-xs md:text-sm font-medium cursor-pointer`}
+        className={` text-gray-600 text-small shadow-[4px_4px_8px_#b8b8b8,-4px_-4px_8px_#ffffff] hover:shadow-[2px_2px_4px_#b8b8b8,-2px_-2px_4px_#ffffff] active:shadow-[inset_2px_2px_4px_#b8b8b8,inset_-2px_-2px_4px_#ffffff] transition-all duration-500 backdrop-blur-sm rounded-lg px-2 py-0.5 ml-2 mr-2 bg-neutral-200 text-sm font-medium cursor-pointer`}
         style={{
           boxShadow: 'inset 2px 2px 4px #f8f8f, inset -2px -2px 4px #ffffff',
         }}
@@ -55,7 +55,7 @@ const LineStatusBadge = ({ statusColor, children }) => {
         {children}
         <button
           onClick={handleCloseModal}
-          className="md:mt-4 px-4 py-1.5 bg-neutral-100/70 text-gray-600 font-extrabold text-small cursor-pointer border-w4 border-neutral-100 hover:shadow-[2px_2px_4px_#b8b8b8,-2px_-2px_4px_#ffffff] active:shadow-[inset_1px_1px_2px_#b8b8b8,inset_-1px_-1px_2px_#ffffff] transition-all duration-500 bg-stone-transparent backdrop-blur-sm rounded-lg"
+          className="text-gray-600 text-small shadow-[4px_4px_8px_#b8b8b8,-4px_-4px_8px_#ffffff] hover:shadow-[2px_2px_4px_#b8b8b8,-2px_-2px_4px_#ffffff] active:shadow-[inset_2px_2px_4px_#b8b8b8,inset_-2px_-2px_4px_#ffffff] transition-all duration-500 backdrop-blur-sm rounded-lg px-2 py-0.5 ml-2 mr-2 bg-neutral-200 text-sm font-medium cursor-pointer"
         >
           close
         </button>
