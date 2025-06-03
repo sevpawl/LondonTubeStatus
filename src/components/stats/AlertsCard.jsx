91a2a33baa47e541fbc94ed1cb73d0c6e05b89ad
sevpawl <savannahpawl@icloud.com>
2025-06-03 14:29:13 -0400

    fix(AlertsCard): make description an array instead of a string to make sure that it can be mapped if there are no issues with any of the lines

--- src/components/stats/AlertsCard.jsx
+++ src/components/stats/AlertsCard.jsx
@@ -20,7 +20,7 @@ const AlertsCard = (lineColor) => {
           description = initialDescription;
           console.log('alerts found');
         } else {
-          description = 'good service on all lines';
+          description = ['good service on all lines'];
           console.log('no alerts found - display ', description);
         }
 
@@ -35,11 +35,11 @@ const AlertsCard = (lineColor) => {
   return (
     <div className="cursor-default w-full max-h-[400px] overflow-hidden px-3 py-2">
       <div className="flex items-center justify-center mb-6 md:mb-6">
-          <span className="text-2xl md:text-3xl not-first:text-neutral-100/80 font-extrabold drop-shadow-[_-3px_-3px_6px_#ffffff80,_4px_4px_2px_#00000033]">
-            alerts
-          </span>
+        <span className="text-2xl md:text-3xl not-first:text-neutral-100/80 font-extrabold drop-shadow-[_-3px_-3px_6px_#ffffff80,_4px_4px_2px_#00000033]">
+          alerts
+        </span>
       </div>
-      <div className='mt-[-9px] overflow-y-auto max-h-[330px]'>
+      <div className="mt-[-9px] overflow-y-auto max-h-[330px]">
         {description.map((alertText, position) => (
           <div
             key={position}
