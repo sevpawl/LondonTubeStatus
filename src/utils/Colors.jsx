9550c25c642a0a85125b5d8d42ab187ec0dc8546
sevpawl <savannahpawl@icloud.com>
2025-06-15 13:18:23 +0100

    feat(Colors): add statusMap map for line status indicator in LineStatusBadge with corresponding colors / icons

--- src/utils/Colors.jsx
+++ src/utils/Colors.jsx
@@ -1,3 +1,12 @@
+import {
+  AlertTriangle,
+  Ban,
+  ThumbsUp,
+  Construction,
+  Hourglass,
+  Hammer,
+} from 'lucide-react';
+
 // status indicator colors
 
 const statusColors = {
@@ -27,4 +36,39 @@ const lineColors = {
   waterlooCity: 'bg-[#99d1bf]',
 };
 
-export { statusColors, lineColors };
+// status map to combine status type, color, icon, icon color
+
+const statusMap = {
+  'good service': {
+    bgColor: 'bg-green-200',
+    icon: ThumbsUp,
+    iconColor: 'text-green-600',
+  },
+  'minor delays': {
+    bgColor: 'bg-yellow-200',
+    icon: Hourglass,
+    iconColor: 'text-yellow-600',
+  },
+  'severe delays': {
+    bgColor: 'bg-red-200',
+    icon: AlertTriangle,
+    iconColor: 'text-red-600',
+  },
+  'part closure': {
+    bgColor: 'bg-orange-200',
+    icon: Construction,
+    iconColor: 'text-orange-600',
+  },
+  'planned': { 
+    bgColor: 'bg-blue-200', 
+    icon: Hammer, 
+    iconColor: 'text-blue-600'
+   },
+    'suspended': { 
+      bgColor: 'bg-gray-300', 
+      icon: Ban, 
+      iconColor: 'text-gray-700'
+   },
+};
+
+export { statusColors, lineColors, statusMap };
