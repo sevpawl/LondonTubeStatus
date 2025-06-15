a156d9b8d62c7c152e0bea1a9af287707026a706
sevpawl <savannahpawl@icloud.com>
2025-06-15 14:14:11 +0100

    feat(LineStatusTile): make a function to fetch line status and import statusMap

--- src/components/lines/LineStatusTile.jsx
+++ src/components/lines/LineStatusTile.jsx
@@ -1,7 +1,25 @@
 import '../../global.css';
 import LineStatusBadge from './LineStatusBadge';
+import { fetchTubeStatus, getStatusByLine } from '../../services/api';
+import { useState, useEffect } from 'react';
+import { statusMap } from '../../utils/Colors';
 
 const LineStatusTile = ({ title, lineColor }) => {
+  const [status, setStatus] = useState('');
+
+  const setLineStatus = async () => {
+    try {
+      const data = await fetchTubeStatus();
+      const lineStatus = getStatusByLine(data);
+      setStatus(lineStatus);
+      console.log('line status: ', lineStatus);
+    } catch (error) {
+      setStatus('unknown');
+      console.log('error fetching line status: ', error);
+    }
+    setLineStatus();
+  };
+
   return (
     <div className="w-full bg-neutral-200 rounded-full p-2 shadow-[8px_8px_16px_#b8b8b8,-8px_-8px_16px_#ffffff] hover:shadow-[2px_2px_4px_#b8b8b8,-2px_-2px_4px_#ffffff] transition-all duration-300">
       <div className="bg-neutral-200 rounded-full overflow-hidden shadow-[inset_4px_4px_8px_#00000033,_inset_-4px_-4px_8px_#ffffff80] flex items-center justify-between w-full px-4 py-2">
@@ -15,7 +33,7 @@ const LineStatusTile = ({ title, lineColor }) => {
           </span>
         </div>
         <div className="flex items-center">
-          <LineStatusBadge>
+          <LineStatusBadge status={status}>
             {
               <a
                 href={`https://tfl.gov.uk/tube/route/${title.toLowerCase()}/`}
@@ -23,7 +41,7 @@ const LineStatusTile = ({ title, lineColor }) => {
                 rel="noopener noreferrer"
                 className="text-gray-600 text-xs hover:text-gray-800"
               >
-                view on TfL site → 
+                view on TfL site →
               </a>
             }
           </LineStatusBadge>
