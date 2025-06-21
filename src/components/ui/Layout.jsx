769a82298d861421a5a340ffc07c96c884197bd2
sevpawl <savannahpawl@icloud.com>
2025-06-21 15:20:00 +0200

    refactor(Layout): update data fetching logic and integrate mapping from constants

--- src/components/ui/Layout.jsx
+++ src/components/ui/Layout.jsx
@@ -6,10 +6,10 @@ import {
   StatsCard,
   AlertsCard,
   ChartCard,
-  Modal,
 } from '../RootContainer';
 import '../../global.css';
-import { fetchTubeStatus } from '../../services/api';
+import { tubeLines } from '../../utils/constants';
+import { fetchTubeStatus, parseLineData } from '../../services/api';
 
 const Layout = () => {
   const [linesData, setLinesData] = useState([]);
@@ -18,9 +18,10 @@ const Layout = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await fetchTubeStatus();
-        setLinesData(data);
+        const parsedData = await fetchTubeStatus();
+        setLinesData(parseLineData(parsedData));
       } catch (error) {
+        console.error('error fetching data: ', error);
         setLinesData([]);
       } finally {
         setIsLoading(false);
@@ -29,9 +30,11 @@ const Layout = () => {
     fetchData();
   }, []);
 
-  // Helper to get line data by name
-  const getLineData = (name) =>
-    linesData.find((line) => line.name.toLowerCase() === name.toLowerCase());
+  // helper to get line data by name
+  const getLineData = (lineName) =>
+    linesData.find(
+      (line) => line.name.toLowerCase() === lineName.toLowerCase(),
+    );
 
   return (
     <div className="flex flex-col items-center min-w-screen px-20">
@@ -41,7 +44,7 @@ const Layout = () => {
           <StatsCard linesData={linesData} />
         </Card>
         <Card>
-          <AlertsCard />
+          <AlertsCard linesData={linesData} />
         </Card>
         <Card>
           <ChartCard />
@@ -49,19 +52,15 @@ const Layout = () => {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-8 max-w-[1460px] w-full px-4 md:px-8 mb-8">
-        <LineStatusTile title="bakerloo" lineColor="#b44a0b" lineData={getLineData('bakerloo')} isLoading={isLoading}></LineStatusTile>
-        <LineStatusTile title="central" lineColor="#e02b24" lineData={getLineData('central')} isLoading={isLoading}></LineStatusTile>
-        <LineStatusTile title="circle" lineColor="#ffcc00" lineData={getLineData('circle')} isLoading={isLoading}></LineStatusTile>
-        <LineStatusTile title="district" lineColor="#008039" lineData={getLineData('district')} isLoading={isLoading}></LineStatusTile>
-        <LineStatusTile title="elizabeth" lineColor="#7055a6" lineData={getLineData('elizabeth')} isLoading={isLoading}></LineStatusTile>
-        <LineStatusTile title="hammersmith & city" lineColor="#d79caf" lineData={getLineData('hammersmith & city')} isLoading={isLoading}></LineStatusTile>
-        <LineStatusTile title="jubilee" lineColor="#9ea7aa" lineData={getLineData('jubilee')} isLoading={isLoading}></LineStatusTile>
-        <LineStatusTile title="metropolitan" lineColor="#a10060" lineData={getLineData('metropolitan')} isLoading={isLoading}></LineStatusTile>
-        <LineStatusTile title="northern" lineColor="#1a1a1a" lineData={getLineData('northern')} isLoading={isLoading}></LineStatusTile>
-        <LineStatusTile title="overground" lineColor="#f17a10" lineData={getLineData('overground')} isLoading={isLoading}></LineStatusTile>
-        <LineStatusTile title="piccadilly" lineColor="#003b8f" lineData={getLineData('piccadilly')} isLoading={isLoading}></LineStatusTile>
-        <LineStatusTile title="victoria" lineColor="#00a3e0" lineData={getLineData('victoria')} isLoading={isLoading}></LineStatusTile>
-        <LineStatusTile title="waterloo & city" lineColor="#99d1bf" lineData={getLineData('waterloo & city')} isLoading={isLoading}></LineStatusTile>
+        {tubeLines.map((line) => (
+          <LineStatusTile
+            key={line.id}
+            lineName={line.name}
+            lineColor={line.color}
+            lineData={getLineData(line.name)}
+            isLoading={isLoading}
+          />
+        ))}
       </div>
     </div>
   );
