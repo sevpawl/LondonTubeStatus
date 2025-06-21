cf78784901196a058fc33e6bdd4e0bba5a0ae084
sevpawl <savannahpawl@icloud.com>
2025-06-21 16:48:00 +0200

    refactor(AlertsCard): fix alert fetching logic, set card title based on alert status, update UI

--- src/components/stats/AlertsCard.jsx
+++ src/components/stats/AlertsCard.jsx
@@ -1,53 +1,40 @@
 import { useEffect, useState } from 'react';
-import { fetchTubeStatus, getStatusDescriptions } from '../../services/api';
-import { lineColors } from '../../utils/constants';
+import { getAlerts } from '../../services/api';
 import '../../global.css';
 
-const AlertsCard = (lineColor) => {
-  const [description, setAlertsDescriptions] = useState([]);
+const AlertsCard = ({ linesData = [] }) => {
+  const [alerts, setAlerts] = useState([]);
 
   useEffect(() => {
+    const alertsData = getAlerts(linesData);
+    setAlerts(alertsData);
+  }, [linesData]);
 
-    const fetchAlertsCardData = async () => {
-      try {
-        const data = await fetchTubeStatus();
-        let initialDescription = getStatusDescriptions(data);
-        console.log('Alerts array:', initialDescription);
-
-        let description;
-        if (initialDescription.length > 0) {
-          description = initialDescription;
-          console.log('alerts found');
-        } else {
-          description = ['good service on all lines'];
-          console.log('no alerts found - display ', description);
-        }
-
-        setAlertsDescriptions(description);
-      } catch (error) {
-        console.log('error fetching alerts card data: ', error);
-      }
-    };
-    fetchAlertsCardData();
-  }, []);
+  const hasAlerts = alerts.length > 0;
 
   return (
     <div className="cursor-default w-full max-h-[400px] overflow-hidden px-3 py-2">
       <div className="flex items-center justify-center mb-6 md:mb-6">
         <span className="text-2xl md:text-3xl not-first:text-neutral-100/80 font-extrabold drop-shadow-[_-3px_-3px_6px_#ffffff80,_4px_4px_2px_#00000033]">
-          alerts
+          {hasAlerts ? 'alerts' : 'no alerts'}
         </span>
       </div>
-      <div className="mt-[-9px] overflow-y-auto max-h-[330px]">
-        {description.map((alertText, position) => (
-          <div
-            key={position}
-            className={`justify-center my-2.5 px-4 py-4 rounded-2xl ${lineColors[lineColor]} text-neutral-600 font-semibold text-sm grid gap-3 bg-neutral-100/80 rounded-lg transition-transform hover:-translate-y-0.5 hover:shadow-lg group cursor-pointer`}
-          >
-            {alertText}
-          </div>
-        ))}
-      </div>
+      {hasAlerts && (
+        <div className="mt-[-9px] overflow-y-auto max-h-[330px]">
+          {alerts.map((alert, position) => (
+            <div
+              key={position}
+              className="my-4 px-4 py-3 rounded-lg transition-transform hover:-translate-y-0.5 hover:shadow-lg cursor-pointer border-l-8 bg-neutral-100/80"
+              style={{ borderLeftColor: alert.color }}
+            >
+              <div className="font-bold text-sm text-gray-800 mb-1">
+                {alert.name}
+              </div>
+              <div className="text-xs text-gray-600">{alert.reason}</div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
